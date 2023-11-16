744683745f5afd204c47d551bbda97ba6e188aaf
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-11-16 21:25:36 +0100

    16/11/23 21h25

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("test code");
+
+const keyMaster = "$2a$10$sIvMxOzunGBB5JXAmSeYjO.3ZdozgJXu7oXypD4UO9GP16Sx6ATBm";
+const binId = "65566e2c12a5d376599a7516";
+const apiUrl = "https://api.jsonbin.io/v3";
+
+
